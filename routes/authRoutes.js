005ad69dad588f1835f911e468e9d6757080ef14
Hem Chandra Upadhyay <hemupadhyay444@gmail.com>
2023-10-17 15:19:29 +0530

    aws s3 use for img gallery

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -130,13 +130,60 @@ router.post("/login", (req, res) => {
 router.get("/profile", requireToken, (req, res) => {
   const current_user = req.user;
 
-  res
-    .status(200)
-    .send({
-      firstName: current_user.firstName,
-      lastName: current_user.lastName,
-      email: current_user.email,
-    });
+  res.status(200).send({
+    firstName: current_user.firstName,
+    lastName: current_user.lastName,
+    email: current_user.email,
+  });
+});
+
+// profile photo request
+
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+aws.config.update({
+  secretAccessKey: process.env.ACCESS_SECRET,
+  accessKeyId: process.env.ACCESS_KEY,
+  region: process.env.REGION,
+});
+
+const BUCKET = process.env.BUCKET;
+const s3 = new aws.S3();
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: BUCKET,
+    key: function (req, file, cb) {
+      console.log(file);
+      cb(null, Date.now()+file.originalname);
+    },
+  }),
+});
+
+router.post("/upload", upload.single("file"), async function (req, res, next) {
+  res.send("Successfully uploaded " + req.file.location + " location!");
+});
+
+router.get("/list", async (req, res) => {
+  let r = await s3.listObjectsV2({ Bucket: BUCKET }).promise();
+  let x = r.Contents.map((item) => item.Key);
+  res.send(x);
+});
+
+router.get("/download/:filename", async (req, res) => {
+  const filename = req.params.filename;
+  let x = await s3.getObject({ Bucket: BUCKET, Key: filename }).promise();
+  res.send(x.Body);
+});
+
+router.delete("/delete/:filename", async (req, res) => {
+  const filename = req.params.filename;
+  await s3.deleteObject({ Bucket: BUCKET, Key: filename }).promise();
+  res.send("File Deleted Successfully");
 });
 
 module.exports = router;
